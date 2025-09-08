decb774074a8f3b83ddfa1e17f8df2a1d200d070
kannareddy8055 <krajashekarreddy0205@gmail.com>
2025-09-08 10:25:29 +0530

    15th commit

--- Authentication/api.js
+++ Authentication/api.js
@@ -12,7 +12,8 @@ const dbUrl = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.gkooz8p.mongodb.
 const cors = require("cors");
 app.use(
   cors({
-    origin: "https://expensify-xi-green.vercel.app",
+    origin:[ "https://expensify-xi-green.vercel.app",
+      "https://expensify-1ahceakgq-rajashekars-projects-a20acbe3.vercel.app"],
     credentials: true,
   })
 );
